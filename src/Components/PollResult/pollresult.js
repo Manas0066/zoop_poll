ef3b56c950418a601760d91fffc09d4d1554c102
Manas0066 <135018682+Manas0066@users.noreply.github.com>
2023-09-01 01:28:59 +0530

    Update pollresult.js

--- src/Components/PollResult/pollresult.js
+++ src/Components/PollResult/pollresult.js
@@ -1,33 +1,32 @@
-import React from 'react'
-import "./pollresult.css"
-function pollresult() {
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function PollResult() {
+  const history = useHistory();
+
+  const handleVote = () => {
+    // Simulate voting action
+    console.log('Voted!');
+  };
+
   return (
-    <>
-       
-       <div className="poll-page-1">
-        <div className="poll-box-result">
-          <div className="poll-question-result">
-            <div className="poll-question-box">Here is your question ?</div>
-          </div>
-          <div className="poll-options">
-
-          <button className='poll-option'>Option 1</button>
-          <button  className='poll-option'>Option 2</button>
-          <button  className='poll-option'>Option 3</button>
-
-          </div>  
-
-         <div className="vote-number">100k Votes</div>
-
+    <div className="poll-page-1">
+      <div className="poll-box-result">
+        <div className="poll-question-result">
+          <div className="poll-question-box">Here is your question ?</div>
         </div>
-
-       
-          <div className="post"><button className='post-button'>Vote</button></div>
-        
+        <div className="poll-options">
+          <button className='poll-option' onClick={handleVote}>Option 1</button>
+          <button className='poll-option' onClick={handleVote}>Option 2</button>
+          <button className='poll-option' onClick={handleVote}>Option 3</button>
+        </div>
+        <div className="vote-number">100k Votes</div>
       </div>
-       
-    </>
-  )
+      <div className="post">
+        <button className='post-button'>Vote</button>
+      </div>
+    </div>
+  );
 }
 
-export default pollresult
+export default PollResult;
