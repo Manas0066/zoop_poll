3e692927ec717d6f92b7f5c054e2342d1ba28656
Manas0066 <135018682+Manas0066@users.noreply.github.com>
2023-09-01 01:28:16 +0530

    Update pollgraph.js

--- src/Components/PollGraph/pollgraph.js
+++ src/Components/PollGraph/pollgraph.js
@@ -1,34 +1,34 @@
-import React from 'react'
-import "./pollgraph.css"
-function pollgraph() {
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function PollGraph() {
+  const history = useHistory();
+
+  const handleEndPoll = () => {
+    // Simulate ending the poll
+    console.log('Poll ended!');
+  };
+
   return (
-    <>
-       <div className="poll-page-2">
-        <div className="poll-box-graph">
-          <div className="poll-question-graph">
-            <div className="poll-question-box-graph">Here is your question ?</div>
-          </div>
-          <div className="poll-options-graph">
-
-          <button className='poll-option-graph'>Option 1</button>
-          <div className="option-flex-with-percent"><div className="option1-poll-percent"></div><div className="percent-option1">50%</div></div>
-          <button  className='poll-option-graph'>Option 2</button>
-          <div className="option-flex-with-percent"><div className="option2-poll-percent"></div><div className="percent-option2">25%</div></div>
-          <button  className='poll-option-graph'>Option 3</button>
-          <div className="option-flex-with-percent"><div className="option3-poll-percent"></div><div className="percent-option3">25%</div></div>
-
-          </div>  
-
-         <div className="vote-number">100k Votes</div>
-
+    <div className="poll-page-2">
+      <div className="poll-box-graph">
+        <div className="poll-question-graph">
+          <div className="poll-question-box-graph">Here is your question ?</div>
         </div>
-
-       
-          <div className="post"><button className='post-button'>End Poll</button></div>
-        
+        <div className="poll-options-graph">
+          <div className="option1-with-graph">
+            <button className='poll-option-graph'>Option 1</button>
+            <span className='percent-option1'>50%</span>
+          </div>
+          {/* ... other options ... */}
+        </div>
+        <div className="vote-number">100k Votes</div>
       </div>
-    </>
-  )
+      <div className="post">
+        <button className='post-button' onClick={handleEndPoll}>End Poll</button>
+      </div>
+    </div>
+  );
 }
 
-export default pollgraph
+export default PollGraph;
