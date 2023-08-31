5d300ac02c37874b8377a7ba5e85b4684f5676d5
Manas0066 <135018682+Manas0066@users.noreply.github.com>
2023-09-01 01:27:00 +0530

    Update poll.js

--- src/Components/Poll/poll.js
+++ src/Components/Poll/poll.js
@@ -1,47 +1,61 @@
+import React, { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { PollContext } from '../../PollContext'; // Correct path to PollContext
+import './poll.css';
 
-import React, { useState } from 'react';
-import "./poll.css";
+function PollApp() {
+  const { pollData, setPollData } = useContext(PollContext);
+  const history = useHistory();
+  const [options, setOptions] = useState(['Option1', 'Option2']); // Options state
 
-const PollApp = () => {
-  const [options, setOptions] = useState(['Option1', 'Option2']);
-  var newOptions;
+  const handlePost = () => {
+    const question = document.querySelector('.poll-question-box').value;
+
+    // Simulate setting poll data
+    setPollData({
+      question,
+      options: options.map(option => ({ text: option, votes: 0 })),
+    });
+
+    // Simulate navigating to the next component
+    history.push('/result');
+  };
+
   const addOption = () => {
-    if(options.length <4 ){
-       newOptions = [...options, `Option${options.length + 1}`];
-      setOptions(newOptions);
+    if (options.length < 4) {
+      const newOption = `Option${options.length + 1}`;
+      setOptions([...options, newOption]);
     }
   };
 
   return (
-    <>
-      <div className="poll-page-1">
-        <div className="poll-box">
-          <div className="poll-question">
-            <input className='poll-question-box' type="text" placeholder='Ask a question ?' />
-          </div>
-          <div className="poll-options">
-            {options.map((option, index) => (
-              <input
-                key={index}
-                type="text"
-                placeholder={option}
-                className="poll-option-input"
-              />
-            ))}
-            {
-              options.length <4 ?   <div className="add-option" onClick={addOption}>
-              +<span className='add-option-span'>Add Option</span>
-            </div>:null
-            }
-          </div>  
+    <div className="poll-page-1">
+      <div className="poll-box">
+        <div className="poll-question">
+          <input className='poll-question-box' type="text" placeholder='Ask a question ?' />
         </div>
-
-       
-          <div className="post"><button className='post-button'>Post</button></div>
-        
+        <div className="poll-options">
+          {options.map((option, index) => (
+            <input
+              key={index}
+              type="text"
+              placeholder={option}
+              className="poll-option-input"
+            />
+          ))}
+          {options.length < 4 && (
+            <div className="add-option" onClick={addOption}>
+              +<span className='add-option-span'>Add Option</span>
+            </div>
+          )}
+        </div>  
       </div>
-    </>
+      <div className="post">
+        <button className='post-button' onClick={handlePost}>Post</button>
+      </div>
+    </div>
   );
-};
+}
 
 export default PollApp;
+
